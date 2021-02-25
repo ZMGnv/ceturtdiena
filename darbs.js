61d82d25709b7091f4579cfbc792bb4e19a66fae
Kristians Fridmanis <kristians.fridmanis@gmail.com>
2021-02-25 11:52:14 +0200

    izveidoju savu bildi

--- darbs.js
+++ darbs.js
@@ -1,5 +1,9 @@
-window.onload{
-    funkction dzest(){
+window.onload(
+    function dzest(){
         
     }
-}
+)
+
+function kfdzest(){
+    document.getElementById("bildekf").src="bildes/kristianst.png"
+    }
